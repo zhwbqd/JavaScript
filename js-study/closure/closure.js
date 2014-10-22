556f90b82211890a5382ce2efdcd73023c9169ca
zhwbqd <zhwbqd@gmail.com>
2014-10-22 23:02:15 +0800

    init

---
+++ js-study/closure/closure.js
@@ -0,0 +1,16 @@
+function Counter(start) {
+    var count = start;
+    return {
+        increment: function() {
+            count++;
+        },
+
+        get: function() {
+            return count;
+        }
+    }
+}
+
+var foo = Counter(4);
+foo.increment();
+foo.get(); // 5
